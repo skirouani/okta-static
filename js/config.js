ba9c36ef5cc822e9c6a8b3fdc5df6f5b5194b099
skirouani <skirouani@hotmail.com>
2026-05-02 23:04:28 +0100

    static okta app

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// ⚠️ Replace these with your actual Okta credentials
+const OKTA_CONFIG = {
+  issuer:   'https://awslab-oie.oktapreview.com/oauth2/default',
+  clientId: '0oa16exr92PrXuAgx1d7'
+};
