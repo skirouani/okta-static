5c18c2f4d422a6e8f2b4ee38af172d08e9e2a28d
skirouani <skirouani@hotmail.com>
2026-05-03 15:56:54 +0100

    fix paths for github pages

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const OKTA_CONFIG = {
   issuer:   'https://awslab-oie.oktapreview.com/oauth2/default',
   clientId: '0oa16exr92PrXuAgx1d7',
-  redirectUri: 'http://localhost:3000/login/callback.html'
+  redirectUri: 'https://skirouani.github.io/okta-static/login/callback.html'
 };
