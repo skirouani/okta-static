bfd97baf3687fb75f070e0305be9a898a2191058
skirouani <skirouani@hotmail.com>
2026-05-03 15:35:39 +0100

    first commit

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-// ⚠️ Replace these with your actual Okta credentials
 const OKTA_CONFIG = {
   issuer:   'https://awslab-oie.oktapreview.com/oauth2/default',
-  clientId: '0oa16exr92PrXuAgx1d7'
+  clientId: '0oa16exr92PrXuAgx1d7',
+  redirectUri: 'http://localhost:3000/login/callback.html'
 };
